7ecb7c229450fd4ddba994baf0015c4d5165aa1d
Erick Tolentino <ericktolentino@pursuit.org>
2023-11-19 09:06:49 -0500

    added

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -8,6 +8,7 @@ const cn = {
     database: process.env.PG_DATABASE,
     user: process.env.PG_USER,
     password: process.env.PG_PASSWORD,
+    databaseURL : process.env.DATABASE_URL,
 };
 
 const db = pgp(cn);
